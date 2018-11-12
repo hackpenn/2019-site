87ba264e7575951a8fa9fdc5ae28455c86f08caf
Zane <github@zane.sh>
2018-11-11 19:59:02 -0800

    begin

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,18 +1,12 @@
 import React from 'react'
-import styled, { injectGlobal } from 'styled-components'
-import { Box, Container, Heading } from '@hackclub/design-system'
+import styled from 'styled-components'
+import { Box, Container, Heading, Button } from '@hackclub/design-system'
 import { theme } from 'theme'
 import Layout from 'components/Layout'
 import Nav from 'components/Nav'
 import BG from 'components/BG'
 import Footer from 'components/Footer'
-
-// awful temp solution for sticky footer, see L53
-injectGlobal`
-  html, body {
-    height: 100%;
-  }
-`
+import Sheet from 'components/Sheet'
 
 const Headline = styled(Heading.h2).attrs({
   color: 'white',
@@ -22,6 +16,36 @@ const Headline = styled(Heading.h2).attrs({
   font-weight: 900;
   line-height: 1.125;
 `
+
+const url = 'http://hackpenn.com/'
+const twitterURL = (text, u = url) =>
+  `https://twitter.com/intent/tweet?text=${text
+    .split(' ')
+    .join('%20')}&url=${u}`
+const facebookURL = (text, u = url) =>
+  `https://www.facebook.com/sharer/sharer.php?u=${u}`
+
+const InlineButton = Button.extend`
+  display: inline-flex;
+  align-items: center;
+  div {
+    background-repeat: no-repeat;
+    background-size: 100%;
+    width: 18px;
+    height: 18px;
+  }
+`
+const ShareButton = ({ children, ...props }) => (
+  <InlineButton
+    target="_blank"
+    aria-label={`Share on ${props.service}`}
+    f={2}
+    {...props}
+  >
+    <Box mr={2} />
+    {children || props.service}
+  </InlineButton>
+)
 
 export default () => (
   <Layout>
@@ -45,14 +69,29 @@ export default () => (
         </Heading.h2>
       </Container>
     </Box.header>
-    <Box
-      style={{
-        position: 'absolute',
-        bottom: 0,
-        width: '100%'
-      }} /* temporary + awful solution for sticky footer */
-    >
-      <Footer />
+    <Box>
+      <Sheet
+        p={[3, 4]}
+        bg={theme.colors.smoke}
+        align="center"
+        maxWidth={24}
+        boxShadowSize="md"
+      >
+        <Heading.h2 f={3} caps mb={2}>
+          Share it!
+        </Heading.h2>
+        <ShareButton
+          service="Twitter"
+          href={twitterURL(
+            'Check out @hackpenn, PA’s largest high school hackathon!',
+            url
+          )}
+          bg="#1da1f2"
+          mr={3}
+        />
+        <ShareButton service="Facebook" href={facebookURL(url)} bg="#3b5998" />
+      </Sheet>
     </Box>
+    <Footer />
   </Layout>
 )
