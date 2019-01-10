dfc1dc4d57c76fbfd55f72dc834d374edad1f52f
Lachlan Campbell <lachlan@hackclub.com>
2019-01-10 12:18:50 -0500

    Redesign schedule

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -9,9 +9,28 @@ import Bannerline from 'components/Bannerline'
 import Headline from 'components/Headline'
 import data from 'data'
 
-const Event = styled(Text).attrs({
-  fontSize: [3, 4]
-})``
+const List = styled(Text.withComponent('ol'))`
+  list-style: none;
+`
+
+const Event = styled(Text.withComponent('li')).attrs({
+  fontSize: [3, 4],
+  pl: 0,
+  mb: 2
+})`
+  line-height: 1.5;
+  strong {
+    display: block;
+  }
+`
+
+const Schedule = styled(Container).attrs({ mx: 0 })`
+  display: grid;
+  grid-gap: ${theme.space[3]}px;
+  ${theme.mediaQueries.md} {
+    grid-template-columns: repeat(2, 1fr);
+  }
+`
 
 export default () => (
   <Layout>
@@ -21,48 +40,33 @@ export default () => (
       color={theme.colors.white}
       px={2}
       pt={[4, 5]}
-      pb={[4, 5, 6]}
+      pb={[3, 4]}
     >
       <Container width={1} px={3}>
         <Headline fontSize={[7, 8]}>Schedule</Headline>
-        <Heading.h2
-          fontSize={[4, 5, 6]}
-          style={{ fontFamily: theme.mono }}
-          mt={3}
-        >
-          24 hours of free food, coding, and fun!
-        </Heading.h2>
       </Container>
     </Box.header>
-    <Box.section bg={theme.colors.snow}>
-      <Container
-        width={1}
-        px={3}
-        pt={3}
-        pb={[4, 5, 6]}
-        color={theme.colors.black}
-      >
-        <Container maxWidth={48} mx={0}>
-          {data.schedule.map(day => (
-            <>
-              <Bannerline />
-              <Headline color={theme.colors.primaryDark}>{day.date}</Headline>
-              <Box mt={3}>
-                {day.events.map(event => (
-                  <Event>
-                    {event.time} —{' '}
-                    <span role="img" aria-label={event.name}>
-                      {event.emoji}
-                    </span>{' '}
-                    {event.name}
-                  </Event>
-                ))}
-              </Box>
-            </>
-          ))}
-        </Container>
-      </Container>
-    </Box.section>
+    <Container width={1} px={3} pb={[4, 5, 6]} color={theme.colors.black}>
+      <Schedule>
+        {data.schedule.map(day => (
+          <Box>
+            <Bannerline mt={5} />
+            <Headline color={theme.colors.primaryDark}>{day.date}</Headline>
+            <List mt={3}>
+              {day.events.map(event => (
+                <Event>
+                  <strong children={event.time} />
+                  <span role="img" aria-label={event.name}>
+                    {event.emoji}
+                  </span>{' '}
+                  {event.name}
+                </Event>
+              ))}
+            </List>
+          </Box>
+        ))}
+      </Schedule>
+    </Container>
     <Footer />
   </Layout>
 )
