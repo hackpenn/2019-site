8da0c3f429a2649f43745f272e501edf93897874
Lachlan Campbell <lachlan@hackclub.com>
2019-01-10 14:28:06 -0500

    Refine Sponsors layout

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Container } from '@hackclub/design-system'
-import data from 'data'
+import { Container, theme } from '@hackclub/design-system'
+import { sponsors } from 'data'
 
 const Base = styled(Container)`
   display: grid;
-  grid-gap: ${props => props.theme.space[3]}px;
+  grid-gap: ${theme.space[2]}px;
   grid-template-columns: repeat(auto-fit, minmax(16rem, 1fr));
   align-items: center;
   justify-content: center;
   img {
-    max-width: 87.5%;
-    max-height: 4rem;
+    max-width: 75%;
+    max-height: 5rem;
   }
 `
 
 const Sponsors = props => (
   <Base {...props}>
-    {data.sponsors.map(sponsor => (
+    {sponsors.map(sponsor => (
       <a href={`${sponsor.link}`}>
         <img
           alt={sponsor.name}
