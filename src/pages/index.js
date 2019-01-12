f4e27fd539586978a22feee0af42c659147e98ec
Zane <github@zane.sh>
2019-01-12 00:10:29 -0800

    Fix right chevron issue on mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -129,7 +129,13 @@ export default () => (
           <Text.span align="left" fontSize={[3, 4]} ml={4} className="mono">
             <strong>Free registration</strong>
             <br />
-            <A.link color="inherit" underline chevronRight to="/location">
+            <A.link
+              color="inherit"
+              fontSize={2}
+              underline
+              chevronRight
+              to="/location"
+            >
               State&nbsp;College, PA
             </A.link>
           </Text.span>
