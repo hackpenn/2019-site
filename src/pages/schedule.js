e8ed5a21f5d09d546a874c56cd1b760256cea48a
Zane <github@zane.sh>
2019-01-12 00:06:07 -0800

    Design work

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -12,42 +12,56 @@ import data from 'data'
 const List = styled(Text.withComponent('ol'))`
   list-style: none;
   padding: 0 2rem;
+  margin: 0 6.5rem;
   border-left: 8px solid ${theme.colors.slate};
-`
 
+  ${theme.mediaQueries.sm} {
+    margin: 0 8rem;
+  }
+`
 const Event = styled(Text.withComponent('li')).attrs({
   fontSize: [3, 4],
-  pl: 0,
-  mb: 2
+  mb: [3, 4]
 })`
   line-height: 1.5;
   position: relative;
 
   &:before {
     content: '';
-    background-color: ${theme.colors.snow};
     height: 2.5rem;
     width: 2.5rem;
-    border-radius: 50%;
     position: absolute;
-    top: 2.1rem;
     left: -3.5rem;
+    background-color: ${theme.colors.snow};
+    border-radius: 50%;
   }
 `
 const Time = styled.strong`
-  display: block;
+  position: absolute;
+  left: -9rem;
+
+  ${theme.mediaQueries.sm} {
+    left: -10.5rem;
+  }
 `
 const Emoji = styled.span.attrs({ role: 'img' })`
   position: absolute;
-  left: -3rem;
+  left: -2.9rem;
+  top: 0.3rem;
+
+  ${theme.mediaQueries.sm} {
+    left: -3rem;
+    top: 0;
+  }
 `
-const Schedule = styled(Container).attrs({ mx: 0 })`
+const Workshop = styled(Text)``
+/*const Schedule = styled(Container).attrs({ mx: 0 })`
   display: grid;
   grid-gap: ${theme.space[3]}px;
   ${theme.mediaQueries.md} {
     grid-template-columns: repeat(2, 1fr);
   }
-`
+`*/
 export default () => (
   <Layout>
     <Nav />
@@ -69,23 +83,23 @@ export default () => (
       </Container>
     </Box.header>
     <Container width={1} px={3} pb={[4, 5, 6]} color={theme.colors.black}>
-      <Schedule>
-        {data.schedule.map(day => (
-          <Box>
-            <Bannerline mt={5} />
-            <Headline color={theme.colors.primaryDark}>{day.date}</Headline>
-            <List mt={3}>
-              {day.events.map(event => (
-                <Event>
-                  <Time children={event.time} />
-                  <Emoji aria-label={event.name}>{event.emoji}</Emoji>
-                  {event.name}
-                </Event>
-              ))}
-            </List>
-          </Box>
-        ))}
-      </Schedule>
+      {/*<Schedule>*/}
+      {data.schedule.map(day => (
+        <Box>
+          <Bannerline mt={5} />
+          <Headline color={theme.colors.primaryDark}>{day.date}</Headline>
+          <List mt={3}>
+            {day.events.map(event => (
+              <Event>
+                <Time children={event.time} />
+                <Emoji aria-label={event.name}>{event.emoji}</Emoji>
+                <Workshop>{event.name}</Workshop>
+              </Event>
+            ))}
+          </List>
+        </Box>
+      ))}
+      {/*</Schedule>*/}
     </Container>
     <Footer />
   </Layout>
